b2231898f236862f4dbf290ed0777875ed7ae319
Ryan Martel <martel.private@gmail.com>
2024-12-01 22:42:26 -0800

    cleaning up some server code

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 import selectors
 import socket
 import json
@@ -10,27 +9,34 @@ from server_lib.action import Action
 from server_lib.message_handler import MessageHandler
 
 class Server:
-    def __init__(self, port, log_level):
-        # Logger options
+    def __init__(self, port: int, log_level) -> None:
+        """Initialize server listening on the given port.
+        Logger is configured as a stdout logger at the given level."""
+        
+        # Logging
         ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
+        ch.setLevel(log_level)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
         self.logger = logging.getLogger('CONNECT-FOUR SERVER')
         self.logger.setLevel(log_level)
         self.logger.addHandler(ch)
 
+        # Socket selectors
         self.port = port
-        self.sock = None
         self.read_sel = selectors.DefaultSelector()
         self.write_sel = selectors.DefaultSelector()
 
+        # Client map
         self.connected_clients = {}
 
+        # Sending actions and receiving handler
         self.action = Action(self.logger)
         self.handler = MessageHandler(self.logger, self.action, self.write_sel, self.connected_clients)
 
-    def start_server(self):
+    def start_server(self) -> None:
+        """ Binding to accept connections from any routable address at the 
+        given port. TCP socket."""
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(('', self.port))
@@ -38,13 +44,18 @@ class Server:
         self.logger.info(f'Started Server at port {self.port}')
         self.read_sel.register(self.sock, selectors.EVENT_READ, self.accept_conn)
         
-    def shutdown(self):
+    def shutdown(self) -> None:
+        """ Server shutdown. Just closes connections, clients 
+        are left to handle this. """
         self.logger.info("Shutting down server")
         for conn in self.connected_clients.keys():
             conn.close()
         self.sock.close()
 
-    def accept_conn(self, sock):
+    def accept_conn(self, sock) -> None:
+        """Accept incoming connections. Connections are refused when there are already
+        two players connected, or the game is in the finished state. The game leaves 
+        the finished state once all players disconnect from the game"""
         conn, addr = sock.accept()
         self.logger.info(f'Accepted client connection from host: {addr[0]}, port: {addr[1]}')
 
@@ -59,12 +70,13 @@ class Server:
             conn.shutdown(socket.SHUT_RDWR)
             return
 
+        # Register client information
         self.connected_clients[conn] = addr
         self.read_sel.register(conn, selectors.EVENT_READ, self.receive)
         self.write_sel.register(conn, selectors.EVENT_WRITE)
         self.handler.new_player_connected(addr)
 
-    def receive(self, sock):
+    def receive(self, sock) -> None:
 
         bmsg_len = sock.recv(4)
         # Client has closed a connection
